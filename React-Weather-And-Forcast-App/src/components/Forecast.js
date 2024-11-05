dcdf5c63a471c90d08afb1f212ae19cd89bc1b96
Zaheer Ahmad Safeer <Zaheer.Ahmed@bellmedex.com>
2024-11-05 10:28:12 -0800

    Change Style of Forcast Module

--- React-Weather-And-Forcast-App/src/components/Forecast.js
+++ React-Weather-And-Forcast-App/src/components/Forecast.js
@@ -63,49 +63,52 @@ function Forecast({ weather }) {
 
   return (
     <div>
-      <div className="city-name">
-        <h2>
-          {data.city}, <span>{data.country}</span>
-        </h2>
-      </div>
-      <div className="date">
-        <span>{getCurrentDate()}</span>
-      </div>
+      {data.condition.icon_url && (
+        <img
+          src={data.condition.icon_url}
+          alt={data.condition.description}
+          className="temp-icon"
+        />
+      )}
       <div className="temp">
-        {data.condition.icon_url && (
-          <img
-            src={data.condition.icon_url}
-            alt={data.condition.description}
-            className="temp-icon"
-          />
-        )}
         {renderTemperature(data.temperature.current)}
         <sup className="temp-deg" onClick={toggleTemperatureUnit}>
           {isCelsius ? "°C" : "°F"} | {isCelsius ? "°F" : "°C"}
         </sup>
       </div>
+      
+      <div className="city-name">
+        <h2>
+          {data.city}, <span>{data.country}</span>
+        </h2>
+      </div>
       <p className="weather-des">{data.condition.description}</p>
+      <div className="date">
+        <span>{getCurrentDate()}</span>
+      </div>
+      
+
       <div className="weather-info">
         <div className="col">
-          <ReactAnimatedWeather icon="WIND" size="40"/>
+          <ReactAnimatedWeather icon="WIND" size="40" />
           <div>
             <p className="wind">{data.wind.speed}m/s</p>
             <p>Wind speed</p>
           </div>
         </div>
         <div className="col">
-          <ReactAnimatedWeather icon="RAIN" size="40"/>
+          <ReactAnimatedWeather icon="RAIN" size="40" />
           <div>
             <p className="humidity">{data.temperature.humidity}%</p>
             <p>Humidity</p>
-        </div>
+          </div>
         </div>
       </div>
       <div className="forecast">
-        <h3>6-Day Forecast:</h3>
+        <h3>Weather Forcast of 4 Days:</h3>
         <div className="forecast-container">
           {forecastData &&
-            forecastData.slice(0, 4).map((day) => (
+            forecastData.slice(1, 5).map((day) => (
               <div className="day" key={day.time}>
                 <p className="day-name">{formatDay(day.time)}</p>
                 {day.condition.icon_url && (
@@ -124,6 +127,6 @@ function Forecast({ weather }) {
       </div>
     </div>
   );
-}        
+}
 
 export default Forecast;
